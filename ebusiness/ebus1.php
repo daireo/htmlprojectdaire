e85a87693d0d5784eb3841f240012447bb4f14f7
Daire O Nuallain <117363273@umail.ucc.ie>
2018-02-27 11:02:14 +0000

    commit

--- ebusiness/ebus1.php
+++ ebusiness/ebus1.php
@@ -3,55 +3,56 @@
     <head>
         <title>Select Product</title>
         
-        <!---jQuery--->
-        <script scr="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script type="text/javascript" scr="cost_calc.js"></script>
-        
+        <!--jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="cost_calc.js"></script>
+
     </head>
     
     <body>
         
-        <h4>Select a product</h4>
-        
-        <br/>
-        
-        <form method="POST" action="Ebus2.php">
-            <label for="salesforce">
-                <input type="radio" id=salesforce name="product" checked onClick="disablebtnProceed()"/>
-                SalesForce @ $200
-                </label>
-                
-                <br/>
-                
-                <label for="ans">
-                 <input type="radio" id=ans name="product" checked onClick="disablebtnProceed()"/>
-                 ANS @ $300
-                 </label>
-                 
-                 <br/>
-                 <br/>
-                 
-                 <label for="subtotal">
-                     Sub Total
-                     <input type="text" id="subtotal" value="0.00" readonly/>
-                     </label>
-                     
-                     <br/>
-                     
-                 <label for="Total">
-                     Total
-                     <input type="text" id="Total" value="0.00" readonly/>
-                     </label>
-                     
-                     <br/>
-                     
-                     <button type="Submit" id="btnProceed" disable>Add to Shopping Cart</button>
-                     
-                     </form>
-                     
-                     <br/>
-                     <button onClick="calSub()">Calculate Cost</button>
-                     <a role="button" href="Ebus1.php">Clear Choice</a><br>
-                 <center><a href="https://htmlprojectdaire-daireo.c9users.io/homepage.html">Homepage</a></center>
+            <h4>Select a Product</h4>
+            
+            <br/>
+            
+            <form method="POST" action="Ebus2.php">
+              
+              <label for="salesforce">
+                <input type="radio" id="salesforce" name="product" checked onClick="disablebtnProceed()"/>
+                SalesForce @ $100
+              </label>
+              
+              <br/>
+              
+              <label for="aws">
+                <input type="radio" id="aws" name="product" onClick="disablebtnProceed()"/>
+                AWS @ $300
+              </label>
+              
+              <br/>
+              <br/>
+              
+              <label for="subtotal">
+                Sub Total
+                <input type="text" id="subtotal" value="0.00" readonly/>
+              </label>
+              
+              <br/>
+              
+              <label for="total">
+                Total
+                <input type="hidden" id="total" name="total" value="0.00" readonly/>
+              </label>
+    
+              <br/>
+              
+              <button type="submit" id="btnProceed" disabled>Add to Shopping Cart</button>
+            
+            </form>
+            
+            <br/>
+            <button onClick="calcSub()">Calculate Cost</button>
+            <a role="button" href="Ebus1.php">Clear Choice</a>
+    
     </body>
 </html>
